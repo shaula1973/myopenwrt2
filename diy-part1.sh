2010c90358830d7a30c1e752c7c55cfe2167c243
shaula1973 <86102828+shaula1973@users.noreply.github.com>
2022-02-24 11:06:43 +0800

    Update diy-part1.sh

--- diy-part1.sh
+++ diy-part1.sh
@@ -17,5 +17,5 @@ echo 'src-git passwall https://github.com/xiaorouji/openwrt-passwall' >>feeds.co
 # echo 'src-git small https://github.com/kenzok8/small' >>feeds.conf.default
 
 # Lienol/openwrt-package
-# sed -i '$a src-git lienol https://github.com/Lancenas/lienol-openwrt-package.git' feeds.conf.default
-# sed -i '$a src-git lienol https://github.com/xiaorouji/openwrt-package' feeds.conf.default
+ sed -i '$a src-git lienol https://github.com/Lancenas/lienol-openwrt-package.git' feeds.conf.default
+ sed -i '$a src-git lienol https://github.com/xiaorouji/openwrt-package' feeds.conf.default
